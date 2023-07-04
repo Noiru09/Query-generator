93463ad3e58903394d1c1405c65112471a3cd182
noiru09 <neelviec@gmail.com>
2023-07-04 11:20:10 +0530

    sql

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import styles from './index.module.css'
+import sqlLogo from './assets/sql-Logo.png'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <main className={styles.main}>
+      <img src={sqlLogo} alt=""className='styles.icon'/>
+      <h3>Generate SQL query with AI</h3>
+
+    <form>
+      <input 
+      type="text"
+      name="Query-Description"
+      placeholder='Describe your query'
+      />
+      <input type="submit" value="Generate Query"/>
+    </form>
+    </main>
+    )
 }
 
 export default App
