032d942e79e24b51cf0de14d10c1f0922d4e14d6
noiru09 <neelviec@gmail.com>
2023-07-06 12:49:16 +0530

    react

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,30 +1,45 @@
-import styles from './index.module.css'
-import sqlLogo from './assets/sql-Logo.png'
-import {useState} from 'react'
+import styles from "./styles.module.css";
+import sqlServer from "./assets/sql-server.png";
+import { useState } from "react";
 
-function App() {
-  const [queryDescription , setQueryDescription] = useState("")
+export default function App() {
+  const [userPrompt, setUserPrompt] = useState("");
+  const [sqlQuery, setSqlQuery] = useState("");
 
-  const onSubmit=(e) =>{
+  const onSubmit = async (e) => {
     e.preventDefault();
-    console.log("Form submitted:",queryDescription)
-  }
+    const query = await generateQuery();
+    setSqlQuery(query);
+  };
+
+  const generateQuery = async () => {
+    const response = await fetch("http://localhost:3002/generate", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ queryDescription: userPrompt }),
+    });
+
+    const data = await response.json();
+    return data.sqlQuery.trim();
+  };
+
   return (
     <main className={styles.main}>
-      <img src={sqlLogo} alt=""className={styles.icon}/>
-      <h3>Generate SQL query with AI</h3>
-
-    <form onSubmit={onSubmit}>
-      <input 
-      type="text"
-      name="Query-Description"
-      placeholder='Describe your query'
-      onChange={(e)=>setQueryDescription(e.target.value)}
-      />
-      <input type="submit" value="Generate Query"/>
-    </form>
+      <img src={sqlServer} className={styles.icon} alt="SQL server" />
+      <h3>Generate SQL</h3>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          name="query-description"
+          placeholder="Describe your query"
+          value={userPrompt}
+          onChange={(e) => setUserPrompt(e.target.value)}
+        />
+        <input type="submit" value="Generate query" />
+      </form>
+      <pre>{sqlQuery}</pre>
     </main>
-    )
+  );
 }
-
-export default App
